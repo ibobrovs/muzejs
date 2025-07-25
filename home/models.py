9353cf967b7159094f27d0e056ea36b6bb72a405
Ivan Bobrov <ibobrovs9@gmail.com>
2025-07-25 21:20:58 +0300

    first pages added

--- home/models.py
+++ home/models.py
@@ -7,15 +7,34 @@ from wagtail import blocks
 from wagtail.fields import StreamField
 from wagtail.images.blocks import ImageChooserBlock
 
-# Страница-список новостей
-class AktualitatesPage(Page):
-    intro = RichTextField(blank=True)
+
+class HomePage(Page):
+    intro = models.CharField(max_length=255, blank=True)
+
+    banner = StreamField([
+        ('banner', blocks.StructBlock([
+            ('title', blocks.CharBlock(required=True)),
+            ('subtitle', blocks.TextBlock(required=False)),
+            ('background_image', ImageChooserBlock(required=False)),
+        ])),
+    ], use_json_field=True, blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel("intro"),
+        FieldPanel('intro'),
+        FieldPanel('banner'),
     ]
+    
+# Страница-список новостей
+class AktualitatesPage(Page):
+    body = StreamField([
+        ("heading", blocks.CharBlock(classname="full title")),
+        ("paragraph", blocks.RichTextBlock()),
+        ("image", ImageChooserBlock()),
+    ], use_json_field=True)
 
-    subpage_types = ['home.AktualitateDetailPage']
+    content_panels = Page.content_panels + [
+        FieldPanel("body")
+    ]
 
 # Страница одной новости
 class AktualitateDetailPage(Page):
@@ -45,4 +64,17 @@ class EkspozicijasPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("intro"),
         FieldPanel("body"),
+    ]
+
+
+class GalerijasPage(Page):
+    gallery = StreamField([
+        ("image", blocks.StructBlock([
+            ("title", blocks.CharBlock(required=False)),
+            ("image", ImageChooserBlock(required=True)),
+        ])),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("gallery"),
     ]
